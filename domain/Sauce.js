9f89e3e23c979aa9a183d2c5e59215b70dd98eb2
Clément BELLIER <clement.bellier@gmail.com>
2022-07-28 14:21:52 +0200

    Renaming variables

--- domain/Sauce.js
+++ domain/Sauce.js
@@ -1,93 +1,103 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const ApiErrors = require('../domain/ApiErrors')
-const Success = require('../domain/Success')
+const ApiErrors = require("../domain/ApiErrors");
+const Success = require("../domain/Success");
 
-const sauceInDB = require('../persistence/sauce')
+const databaseSauceAccess = require("../persistence/sauce");
 
 class Sauce {
-    constructor(){}
-    createSauce = async (sauce) => {
-        const sauceToSave = {
-            ...sauce,
-            likes: 0,
-            dislikes: 0,
-            usersLiked: [],
-            usersDisliked: []
-        }
-        const savedSauve = await sauceInDB.saveSauceInDB(sauceToSave)
-        if(savedSauve.error) return new ApiErrors(savedSauve.error).serverError()
-        return new Success().sauceCreated()
-    }
-    findAll = async () => {
-        const sauces = await sauceInDB.findAllSauces()
-        if(sauces.error) return new ApiErrors(sauces.error).serverError()
-        return new Success().sauceFound(sauces)
-    }
-    findOne = async (sauceId) => {
-        const sauce = await sauceInDB.findOneSauce(sauceId)
-        if(!sauce) return new ApiErrors().sauceNotFound()
-        if(sauce.error) return new ApiErrors(sauce.error).serverError()
-        return new Success().sauceFound(sauce)
-    }
-    deleteImage = sauce => {
-        const fileName = sauce.imageUrl.split('/images/')[1]
-        fs.unlink(`images/${fileName}`, ()=>{})
-    }
-    deleteSauce = async (sauceId, userWhoAskDelete) => {
-        const sauce = await sauceInDB.findOneSauce(sauceId)
-        if(sauce.error) return new ApiErrors(sauce.error).notFound()
-        if(sauce.userId !== userWhoAskDelete) return new ApiErrors().unauthorizedRequest()
+  constructor() {}
+  createSauce = async (sauce) => {
+    const sauceToSave = {
+      ...sauce,
+      likes: 0,
+      dislikes: 0,
+      usersLiked: [],
+      usersDisliked: [],
+    };
+    const savedSauve = await databaseSauceAccess.saveSauceInDB(sauceToSave);
+    if (savedSauve.error) return new ApiErrors(savedSauve.error).serverError();
+    return new Success().sauceCreated();
+  };
+  findAll = async () => {
+    const sauces = await databaseSauceAccess.findAllSauces();
+    if (sauces.error) return new ApiErrors(sauces.error).serverError();
+    return new Success().sauceFound(sauces);
+  };
+  findOne = async (sauceId) => {
+    const sauce = await databaseSauceAccess.findOneSauce(sauceId);
+    if (!sauce) return new ApiErrors().sauceNotFound();
+    if (sauce.error) return new ApiErrors(sauce.error).serverError();
+    return new Success().sauceFound(sauce);
+  };
+  deleteImage = (sauce) => {
+    const fileName = sauce.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${fileName}`, () => {});
+  };
+  deleteSauce = async (sauceId, userWhoAskDelete) => {
+    const sauce = await databaseSauceAccess.findOneSauce(sauceId);
+    if (sauce.error) return new ApiErrors(sauce.error).notFound();
+    if (sauce.userId !== userWhoAskDelete)
+      return new ApiErrors().unauthorizedRequest();
 
-        this.deleteImage(sauce)
-        const deletedSauce = await sauceInDB.deleteSauce(sauceId)
-        if(deletedSauce.error) return new ApiErrors(deletedSauce).serverError()
-        return new Success().sauceDeleted()        
-    }
-    updateSauce = async (sauce) => {
-        return await sauceInDB.modifySauce(sauce)
-    }
-    modifySauce = async (modifiedSauce, userWhoAskModify) => {
-        const originSauce = await sauceInDB.findOneSauce(modifiedSauce._id)
-        if(originSauce.error) return new ApiErrors(originSauce.error).notFound()
-        if(originSauce.userId !== userWhoAskModify) return new ApiErrors().unauthorizedRequest()
-        
-        if(modifiedSauce.imageUrl) this.deleteImage(originSauce)
-        const updatedSauce = await this.updateSauce(modifiedSauce)
-        if(updatedSauce.error) return new ApiErrors(updatedSauce.error).serverError()
-        return new Success().sauceModified()
-    }
-    unlike = async (userId, sauce) => {
-        if(sauce.usersLiked.includes(userId)){
-            sauce.usersLiked = sauce.usersLiked.filter(id => id !== userId)
-            sauce.likes--
-        }
-        if(sauce.usersDisliked.includes(userId)){
-            sauce.usersDisliked = sauce.usersDisliked.filter(id => id !== userId)
-            sauce.dislikes--
-        }
-        const unlikeSauce = await this.updateSauce(sauce)
-        if(unlikeSauce.error) return new ApiErrors(unlikeSauce.error).serverError()
-        return new Success().unlikeRecord()
-    }
+    this.deleteImage(sauce);
+    const deletedSauce = await databaseSauceAccess.deleteSauce(sauceId);
+    if (deletedSauce.error) return new ApiErrors(deletedSauce).serverError();
+    return new Success().sauceDeleted();
+  };
+  updateSauce = async (sauce) => {
+    return await databaseSauceAccess.modifySauce(sauce);
+  };
+  modifySauce = async (modifiedSauce, userWhoAskModify) => {
+    const originSauce = await databaseSauceAccess.findOneSauce(
+      modifiedSauce._id
+    );
+    if (originSauce.error) return new ApiErrors(originSauce.error).notFound();
+    if (originSauce.userId !== userWhoAskModify)
+      return new ApiErrors().unauthorizedRequest();
 
-    likeSauce = async (userId, like, sauceId) => {
-        const sauce = await sauceInDB.findOneSauce(sauceId)
-        if(sauce.error) return new ApiErrors(sauce.error).notFound()
-        if(like === 0) return await this.unlike(userId, sauce)
-        if(sauce.usersLiked.includes(userId) || sauce.usersDisliked.includes(userId)) return new ApiErrors("L'utilisateur-rice a déjà voté").badRequest()
-        if(like === 1) {
-            sauce.likes ++
-            sauce.usersLiked.push(`${userId}`)
-        }
-        if(like === -1) {
-            sauce.dislikes ++
-            sauce.usersDisliked.push(`${userId}`)
-        }
-        const likeSauce = await this.updateSauce(sauce)
-        if(likeSauce.error) return new ApiErrors(likeSauce.error).serverError()
-        return new Success().likeRecord()
+    if (modifiedSauce.imageUrl) this.deleteImage(originSauce);
+    const updatedSauce = await this.updateSauce(modifiedSauce);
+    if (updatedSauce.error)
+      return new ApiErrors(updatedSauce.error).serverError();
+    return new Success().sauceModified();
+  };
+  unlike = async (userId, sauce) => {
+    if (sauce.usersLiked.includes(userId)) {
+      sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId);
+      sauce.likes--;
     }
+    if (sauce.usersDisliked.includes(userId)) {
+      sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId);
+      sauce.dislikes--;
+    }
+    const unlikeSauce = await this.updateSauce(sauce);
+    if (unlikeSauce.error)
+      return new ApiErrors(unlikeSauce.error).serverError();
+    return new Success().unlikeRecord();
+  };
+
+  likeSauce = async (userId, like, sauceId) => {
+    const sauce = await databaseSauceAccess.findOneSauce(sauceId);
+    if (sauce.error) return new ApiErrors(sauce.error).notFound();
+    if (like === 0) return await this.unlike(userId, sauce);
+    if (
+      sauce.usersLiked.includes(userId) ||
+      sauce.usersDisliked.includes(userId)
+    )
+      return new ApiErrors("L'utilisateur-rice a déjà voté").badRequest();
+    if (like === 1) {
+      sauce.likes++;
+      sauce.usersLiked.push(`${userId}`);
+    }
+    if (like === -1) {
+      sauce.dislikes++;
+      sauce.usersDisliked.push(`${userId}`);
+    }
+    const likeSauce = await this.updateSauce(sauce);
+    if (likeSauce.error) return new ApiErrors(likeSauce.error).serverError();
+    return new Success().likeRecord();
+  };
 }
 
-module.exports = Sauce
+module.exports = Sauce;
--- domain/User.js
+++ domain/User.js
@@ -1,44 +1,55 @@
-const crypt = require('../encryption/crypt')
-const tokenBuilder = require('../encryption/token')
+const crypt = require("../encryption/crypt");
+const tokenBuilder = require("../encryption/token");
 
-const ApiErrors = require('../domain/ApiErrors')
-const Success = require('../domain/Success')
+const ApiErrors = require("../domain/ApiErrors");
+const Success = require("../domain/Success");
 
-const userInDB = require('../persistence/user')
+const databaseUserAccess = require("../persistence/user");
 
 class User {
-    constructor(email, password){
-        this.email = email
-        this.password = password
-        this.userId = ""
-    }
-    saveUser = async () => {
-        const encryptPassword = await crypt.cryptPassword(this.password)
-        if(encryptPassword.error) return new ApiErrors(encryptPassword.error).serverError()
-        this.password = encryptPassword.hash
-        const savedUser = await userInDB.saveUserInDB(this.email, this.password)
-        if(savedUser.error) return new ApiErrors(savedUser.error).badRequest()
-        return new Success().userCreated()        
-    }
-    findUser = async () => {
-        const isUserInDB = await userInDB.findUserInDB((this.email))
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+    this.userId = "";
+  }
+  saveUser = async () => {
+    const encryptPassword = await crypt.cryptPassword(this.password);
+    if (encryptPassword.error)
+      return new ApiErrors(encryptPassword.error).serverError();
+    this.password = encryptPassword.hash;
+    const savedUser = await databaseUserAccess.saveUserInDB(
+      this.email,
+      this.password
+    );
+    if (savedUser.error) return new ApiErrors(savedUser.error).badRequest();
+    return new Success().userCreated();
+  };
+  findUser = async () => {
+    const userInDatabase = await databaseUserAccess.findUserInDB(
+      this.email
+    );
 
-        if(!isUserInDB) return new ApiErrors().userNotFound()
-        if(isUserInDB.error) return new ApiErrors(isUserInDB.error).serverError()
+    if (!userInDatabase) return new ApiErrors().userNotFound();
+    if (userInDatabase.error)
+      return new ApiErrors(userInDatabase.error).serverError();
 
-        this.userId = isUserInDB._id
-        const validPassword = await crypt.comparePassword(this.password, isUserInDB.password)
+    this.userId = userInDatabase._id;
+    const validPassword = await crypt.comparePassword(
+      this.password,
+      userInDatabase.password
+    );
 
-        if(!validPassword) return new ApiErrors().wrongPassword()
+    if (!validPassword) return new ApiErrors().wrongPassword();
 
-        const token = tokenBuilder.signToken(this.userId)
-        return new Success().userFound(this.userId, token)
-    }
-    authorize = (token) => {
-        const decodedToken = tokenBuilder.verifyToken(token)
-        if(!decodedToken || decodedToken.error) return new ApiErrors('Requête non authentifiée !').unauthorized()
-        return {code: 200, userId: decodedToken.userId}
-    }
+    const token = tokenBuilder.signToken(this.userId);
+    return new Success().userFound(this.userId, token);
+  };
+  authorize = (token) => {
+    const decodedToken = tokenBuilder.verifyToken(token);
+    if (!decodedToken || decodedToken.error)
+      return new ApiErrors("Requête non authentifiée !").unauthorized();
+    return { code: 200, userId: decodedToken.userId };
+  };
 }
 
-module.exports = User
+module.exports = User;
